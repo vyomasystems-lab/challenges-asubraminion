1d4ca7841264601f1c1d3092b351d5cd9120ef31
Anushka Subramanian <anushka.subraminion@gmail.com>
2022-07-26 07:53:17 +0000

    update 1

--- level1_design2/test_seq_detect_1011.py
+++ level1_design2/test_seq_detect_1011.py
@@ -23,4 +23,39 @@ async def test_seq_bug1(dut):
     dut.reset.value = 0
     await FallingEdge(dut.clk)
 
-    cocotb.log.info('#### CTB: Develop your test here! ######')
+    ip=random.randint(0,1)
+    ip1=random.randint(0,1)
+    ip2=random.randint(0,1)
+    ip3=random.randint(0,1)
+
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=ip
+    if (ip==1)
+        STATE1=1
+    else STATE1=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=ip1
+    if (STATE1==1)
+        if (ip1==1)
+            STATE10=0
+        else STATE10=1
+    else 
+        if (ip1==1)
+            STATE1=1
+        else STATE1=0
+    await FallingEdge(dut.clk)
+    dut.inp_bit.value=ip2
+    if (STATE10==1)
+        if (ip2==1)
+            STATE101=1
+        else STATE1=0
+    else 
+        if (ip1==1)
+            STATE1=1
+        else STATE1=0
+
+
+    
+
+    assert dut.seq_seen.value==expout, "Randomised test failed, for input {A}. Expected output {B}, actual output {C}".format(A=SEL, B=expout, C=dut.seq_seen.value)
+#await 2 ns?
